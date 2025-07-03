00e691baf875c32f7d5694e2cbcd2138be5f8c28
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2025-07-03 12:30:41 +0200

    12.30

---
+++ script.js
@@ -0,0 +1,20 @@
+//Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.
+
+let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
+
+
+let frase = array_1[0][0]+" "+array_1[1][0]+" "+array_1[0][1]+" "+array_2[4][0][0]+" "+array_1[0][0]+" "+array_1[1][0]+" "+array_1[0][1]+" "+array_1[1][1]+" "+array_1[0][0]+" "+array_1[1][0]+" "+array_1[0][1]+" "+array_1[2][0]+" "+array_1[2][1]+ ' ' +array_2[3][0]+" "+array_2[4][1]+" "+array_1[0][2];
+console.log(frase)
+
